9a4fbf3ff96b48e62d3e9006800b7d290b2eb726
Robert Birch <robdbirch@gmail.com>
2014-05-03 13:29:21 -0400

    File testing pretty much complete

--- spec/snooper_spec.rb
+++ spec/snooper_spec.rb
@@ -54,7 +54,7 @@ describe 'Snooper' do
       expect { @snooper.load_array [ @jsnoopy ] }.to raise_error ArgumentError
     end
 
-    it 'loads a snoopies json file and sniffs out a log file containing Non OK Status'  do
+    it 'loads a snoopies json file and sniffs out a log file' do
       @snooper.load_array [ @jsnoopy ]
       @snooper.snoopies.size.should == 1
       @snooper.snoopies[0].sniffers.size.should == 3
@@ -63,10 +63,135 @@ describe 'Snooper' do
         snoopy.sniffers.each do |sniffer|
           if sniffer.regexp.source == 'Non OK Status'
             sniffer.sniffed.size.should eq 60
+            sniffer.sniffed.each do |sniffed|
+              sniffed.before.size.should eq 2
+              sniffed.after.size.should eq 2
+            end
           elsif sniffer.regexp.source == 'Failed to bulk load'
             sniffer.sniffed.size.should eq 1
+            sniffer.sniffed.each do |sniffed|
+              sniffed.before.size.should eq 2
+              sniffed.after.size.should eq 2
+            end
           elsif sniffer.regexp.source == 'Total Number of records:'
             sniffer.sniffed.size.should eq 47
+            sniffer.sniffed.each do |sniffed|
+              sniffed.before.size.should eq 1
+              sniffed.after.size.should eq 1
+            end
+          else
+            # This should not happen
+            true.should eq false
+          end
+        end
+      end
+    end
+
+    it 'loads a snoopies json file and sniffs out a log file zero before zero after' do
+      @jsnoopy['sniffers'].each do |sniffer|
+        sniffer['lines']['before'] = 0
+        sniffer['lines']['after'] = 0
+      end
+      @snooper.load_array [ @jsnoopy ]
+      @snooper.snoopies.size.should == 1
+      @snooper.snoopies[0].sniffers.size.should == 3
+      snoopies = @snooper.snoop
+      snoopies.each do |snoopy|
+        snoopy.sniffers.each do |sniffer|
+          if sniffer.regexp.source == 'Non OK Status'
+            sniffer.sniffed.size.should eq 60
+            sniffer.sniffed.each do |sniffed|
+              sniffed.before.size.should eq 0
+              sniffed.after.size.should eq 0
+            end
+          elsif sniffer.regexp.source == 'Failed to bulk load'
+            sniffer.sniffed.size.should eq 1
+            sniffer.sniffed.each do |sniffed|
+              sniffed.before.size.should eq 0
+              sniffed.after.size.should eq 0
+            end
+          elsif sniffer.regexp.source == 'Total Number of records:'
+            sniffer.sniffed.size.should eq 47
+            sniffer.sniffed.each do |sniffed|
+              sniffed.before.size.should eq 0
+              sniffed.after.size.should eq 0
+            end
+          else
+            # This should not happen
+            true.should eq false
+          end
+        end
+      end
+    end
+
+
+    it 'loads a snoopies json file and sniffs out a log file 0 before 1 after', :focus do
+      @jsnoopy['sniffers'].each do |sniffer|
+        sniffer['lines']['before'] = 0
+        sniffer['lines']['after'] = 1
+      end
+      @snooper.load_array [ @jsnoopy ]
+      @snooper.snoopies.size.should == 1
+      @snooper.snoopies[0].sniffers.size.should == 3
+      snoopies = @snooper.snoop
+      snoopies.each do |snoopy|
+        snoopy.sniffers.each do |sniffer|
+          if sniffer.regexp.source == 'Non OK Status'
+            sniffer.sniffed.size.should eq 60
+            sniffer.sniffed.each do |sniffed|
+              sniffed.before.size.should eq 0
+              sniffed.after.size.should eq 1
+            end
+          elsif sniffer.regexp.source == 'Failed to bulk load'
+            sniffer.sniffed.size.should eq 1
+            sniffer.sniffed.each do |sniffed|
+              sniffed.before.size.should eq 0
+              sniffed.after.size.should eq 1
+            end
+          elsif sniffer.regexp.source == 'Total Number of records:'
+            sniffer.sniffed.size.should eq 47
+            sniffer.sniffed.each do |sniffed|
+              sniffed.before.size.should eq 0
+              sniffed.after.size.should eq 1
+            end
+          else
+            # This should not happen
+            true.should eq false
+          end
+        end
+      end
+    end
+
+
+    it 'loads a snoopies json file and sniffs out a log file 1 before 0 after', :focus do
+      @jsnoopy['sniffers'].each do |sniffer|
+        sniffer['lines']['before'] = 1
+        sniffer['lines']['after'] = 0
+      end
+      @snooper.load_array [ @jsnoopy ]
+      @snooper.snoopies.size.should == 1
+      @snooper.snoopies[0].sniffers.size.should == 3
+      snoopies = @snooper.snoop
+      snoopies.each do |snoopy|
+        snoopy.sniffers.each do |sniffer|
+          if sniffer.regexp.source == 'Non OK Status'
+            sniffer.sniffed.size.should eq 60
+            sniffer.sniffed.each do |sniffed|
+              sniffed.before.size.should eq 1
+              sniffed.after.size.should eq 0
+            end
+          elsif sniffer.regexp.source == 'Failed to bulk load'
+            sniffer.sniffed.size.should eq 1
+            sniffer.sniffed.each do |sniffed|
+              sniffed.before.size.should eq 1
+              sniffed.after.size.should eq 0
+            end
+          elsif sniffer.regexp.source == 'Total Number of records:'
+            sniffer.sniffed.size.should eq 47
+            sniffer.sniffed.each do |sniffed|
+              sniffed.before.size.should eq 1
+              sniffed.after.size.should eq 0
+            end
           else
             # This should not happen
             true.should eq false
@@ -76,7 +201,7 @@ describe 'Snooper' do
     end
 
     it 'Dump trace'  do
-      dump_it
+      #dump_it
     end
 
     def dump_it
